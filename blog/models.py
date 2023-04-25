bd099a5a3d89c2663598a9fc326a396cb6a1b5fd
JamesOmare <jamesomare922@gmail.com>
2023-04-25 15:09:21 +0300

    initial commit

---
+++ blog/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+
+    # This custom manager which will run postObjects 
+    # by default on query to filter out all except published posts
+    class PostObjects(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset() .filter(status='published')
+
+    options = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT, default=1)
+    title = models.CharField(max_length=250)
+    excerpt = models.TextField(null=True)
+    content = models.TextField()
+    slug = models.SlugField(max_length=250, unique_for_date='published')
+    published = models.DateTimeField(default=timezone.now)
+
+    # cascade means if we delete a user it will also delete the post made by the user
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts')
+    status = models.CharField(
+        max_length=10, choices=options, default='published')
+    objects = models.Manager()  # default manager
+    postobjects = PostObjects()  # custom manager
+
+    class Meta:
+        ordering = ('-published',)
+
+    def __str__(self):
+        return self.title
